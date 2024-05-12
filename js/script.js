4e45ad436c12934696bbb5dd06e55ff7933252bb
martinaliporata <martinaliporata@gmail.com>
2024-05-12 20:38:20 +0200

    inserito ultimo if

--- js/script.js
+++ js/script.js
@@ -3,5 +3,6 @@ for (let i=0; i <= 100; i++) {
         console.log('Fizz')
     } else if(i%5 === 0) {
         console.log('Buzz')
-    }
+    } else (i%3 === 0 && i%5 === 0)
+        console.log('FizzBuzz')
 }
